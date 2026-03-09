8235aa66b7e299158c527d0ca3407460c3f61709
heisenshark <heisenshark@gmail.com>
2026-03-09 20:45:51 +0100

    feat: added day4 part2 solution

--- day4/solution.py
+++ day4/solution.py
@@ -12,40 +12,54 @@ checkpositions = [
         ]
 with open("data.txt",'r') as file:
     lines = file.readlines()
-# lines = '''..@@.@@@@.
-# @@@.@.@.@@
-# @@@@@.@.@@
-# @.@@@@..@.
-# @@.@@@@.@@
-# .@@@@@@@.@
-# .@.@.@.@@@
-# @.@@@.@@@@
-# .@@@@@@@@.
-# @.@.@@@.@.'''.split("\n")
-print(lines)
 
-dimensions = [len(lines[0])-1,len(lines)]
+lines_rep = []
+for i in lines:
+    arr = []
+    for j in i:
+        if j=='\n':
+           continue
+        arr.append(j=='@')
+    lines_rep.append(arr)
 
-def checkCase(x,y,lines):
-    sum = 0
-    for i in checkpositions:
-        poschecked = [x+i[0],y+i[1]]
-        if not 0<=poschecked[0] < dimensions[0] or not 0<=poschecked[1]<dimensions[1]:
-            continue
-        if lines[poschecked[1]][poschecked[0]] == '@':
-            sum+=1
-        print([poschecked,sum,lines[poschecked[1]][poschecked[0]]])
-    print(sum)
-    if sum<4:
-        return True
-    return False
+dimensions = [len(lines_rep[0]),len(lines_rep)]
 
-s = 0
-for i in range(0,dimensions[1]):
-    for j in range(0,dimensions[0]):
-        if(lines[i][j] == '.'):
-            continue
-        res = checkCase(j,i,lines)
-        print([res,[i,j]])
-        s+=1*(res==1)
-print(s)
+def runRemoval(lines):
+    def checkCase(x,y,lines):
+        sum = 0
+        for i in checkpositions:
+            poschecked = [x+i[0],y+i[1]]
+            if not 0<=poschecked[0] < dimensions[0] or not 0<=poschecked[1]<dimensions[1]:
+                continue
+            if lines[poschecked[1]][poschecked[0]] == True:
+                sum+=1
+        if sum<4:
+            return True
+        return False
+
+    s = 0
+    removal_array = []
+    for i in range(0,dimensions[1]):
+        for j in range(0,dimensions[0]):
+            if(lines[i][j] == False):
+                continue
+            res = checkCase(j,i,lines)
+            if res:
+                removal_array.append([j,i])
+            s+=1*(res==1)
+
+            # print([res,[i,j]])
+    for i in removal_array:
+        lines[i[1]][i[0]] = False
+    return s 
+
+count_all = 0
+while True:
+    count = runRemoval(lines_rep)
+    if(count == 0):
+        break
+    if count_all == 0:
+        print(count)
+    count_all += count
+
+print(count_all)
