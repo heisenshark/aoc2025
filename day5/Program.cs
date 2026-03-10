9d1a309940f275a74345749c8acb7aed3c4446f0
heisenshark <heisenshark@gmail.com>
2026-03-10 22:54:34 +0100

    feat: added day5 cs

---
+++ day5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("test elo");
+
+var file = File.Open("data.txt", FileMode.Open);
+var reader = new StreamReader(file);
+var currentLine = "";
+var lineList = new List<string>();
+
+while (!reader.EndOfStream&& currentLine !="\n")
+{
+    currentLine = reader.ReadLine();
+    if(currentLine!=null)
+        lineList.Add(currentLine);  
+}
+
+Console.WriteLine(lineList);  
+file.Close();
